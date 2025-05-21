6341c78032701fab4cd0a11affb09ce545146379
Oleksandr Nemesh <prevter@gmail.com>
2025-05-21 22:49:51 +0300

    bump to v0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_namespace_packages
 
 setup(
     name="pybroma",
-    version="0.1.0",
+    version="0.1.1",
     author="Calloc",
     packages=find_namespace_packages(include="pybroma*"),
     cmake_install_dir="pybroma",
